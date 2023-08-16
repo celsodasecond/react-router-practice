c35d3fa2e37ae8b70a62056b5ba5f60a29c13b48
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-16 21:29:33 +0800

    converted cards to use redux

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Card extends Component {
+class Card extends Component {
+	// state = { user: "" };
 
-    state = { user: '' }
+	// componentDidMount() {
+	// 	let user = this.props.match.params.user;
+	// 	this.setState({ user }); // ES2015 Allows the use of single variable since the key and value is the same.
+	// }
 
-    componentDidMount() {
-        let user = this.props.match.params.user 
-        this.setState({user}) // ES2015 Allows the use of single variable since the key and value is the same.
-    }
-
-  render() {
-    const { user } = this.state
-    return (
-        <div
-            className='ui raised very padded text container segment' style={{ marginTop: '80px' }}>
-            <h3 className='ui header'>{ user.toUpperCase() }</h3>
-        </div>
-    )
-  }
+	render() {
+		// const { user } = this.state;
+		const { title, body } = this.props.card;
+		return (
+			<div
+				className="ui raised very padded text container segment"
+				style={{ marginTop: "80px" }}>
+				<h3 className="ui header">{title}</h3>
+				<p>{body}</p>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state, ownProps) => {
+	let title = ownProps.match.params.user;
+	return {
+		card: state.cards.find((card) => card.title === title),
+	};
+};
+
+export default connect(mapStateToProps)(Card);
